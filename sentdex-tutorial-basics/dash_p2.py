2e2f439f124a49e27cede48aa6617540c4e6589d
Oryx <vgoyhex@gmail.com>
2019-02-11 14:44:51 +0100

    added more of the tutorial basics scripts

---
+++ sentdex-tutorial-basics/dash_p2.py
@@ -0,0 +1,27 @@
+#https://www.youtube.com/watch?v=hRH01ZzT2NI
+
+import dash
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash()
+app.layout = html.Div(children=[
+    html.H1('Dash-tutorials'),
+    dcc.Input(id='input', value='Enter Something', type='text'),
+    html.Div (id='output')
+              ])
+
+@app.callback(
+        Output(component_id='output', component_property='children'),
+        [Input(component_id='input', component_property='value')])
+
+def update_value(input_data):
+    try:
+        return str(float(input_data)**2)
+    except:
+        return "some error occured, oops?"
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+
